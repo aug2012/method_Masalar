e1ae59b7e1c0c94ae3a35eab527cc614eae5f024
Firdavs <Xaker.pro.N>
2025-11-12 17:57:20 +0500

    Masalalar yechildi

---
+++ app.js
@@ -0,0 +1,161 @@
+// 1-TEST
+
+// 1-masala
+// let fruits=['apple','banana','orange'];
+// fruits.push('peach');
+// console.log(fruits)
+
+// 2-masala
+// let nums=[4,5,6,7];
+// nums.pop();
+// console.log(nums);
+
+// 3-masala
+// let arr=['middle','end'];
+// arr.unshift('start');
+// console.log(arr);
+
+// 4-masala
+// let cars=['BMW','Mercedes','Tesla'];
+// cars.shift();
+// console.log(cars);
+
+// 5-masala
+// let a=[1,2];
+// let b=[3,4,5];
+// let c=a.contact(b);
+// console.log(c);
+
+// 6-masala
+// let fruits=["apple","banana","orange","kiwi"];
+// let sitrus=fruits.slice(1,2);
+// console.log(sitrus);
+
+// 7-masala
+// let tech=["HTML","CSS","JS","React"];
+// tech.splice(2,1);
+// console.log(tech);
+
+// 8-masala
+// let langs=['HTML','CSS','JavaScript'];
+// console.log(langs.indexOf('CSS'));
+
+// 9-masala
+// let fruits=['apple','banana','apple','orange'];
+// console.log(fruits.lastindexOf('apple'));
+
+// 10-masala
+// let fruits=['apple','banana','orange'];
+// console.log(fruits.includeds('kiwi'));
+
+// 11-masala
+// let nums=[3,8,12,5,20];
+// console.log(nums.find(nums=>nums>10));
+
+// 12-masala
+// let ages=[12,15,22,16];
+// console.log(ages.some(ages=>ages>18));
+
+// 13-masala
+// let nums=[4,6,8,-2];
+// console.log(nums.every(nums=>nums>0));
+
+// 14-masala
+// let nums=[1,2,3];
+// console.log(nums.map(nums=>nums*3));
+
+// 15-masala
+// let words=['cat','elephant','dog','mouse'];
+// console.log(words.filter(words=>words.length>4));
+
+// 16-masala
+// let nums=[5,10,15];
+// console.log(nums.reduce((total,num)=>total+num,0));
+
+// 17-masala
+// let names=['Zarina','Ali','Bekzod'];
+// console.log(names.sort());
+
+// 18-masala
+// let nums=[40,5,25,100];
+// console.log(nums.sort((a,b)=>a-b));
+
+// 19-masala
+// let arr=[1,2,3,4];
+// console.log(arr.reverse());
+
+// 20-masala
+// let nums=[-3,4,-1,6,0];
+// let musbat=nums.filter(nums=>nums>0).map(nums=>nums*2).reverse();
+// console.log(musbat);
+
+// 2-TEST
+
+// 1-masala
+// let fruits=["apple","banana","orange","kiwi"];
+// fruits.splice(1,1);
+// console.log(fruits);
+
+// 2-masala
+// let nums=[2,4,6,8];
+// let nums_after=nums.map(nums=>nums*3).reduce((total,num)=>total+num,0);
+// console.log(nums_after);
+
+// 3-masala
+// let names=['Zarina','Ali','Bekzod','Dilshod'];
+// let names2=names.sort().reverse()
+// console.log(names2);
+
+// 4-masala
+// let nums=[-3,5,-1,0,8,-7];
+// let filter=nums.filter(nums=>nums>0);
+// console.log(filter);
+
+// 5-masala
+// let nums=[4,9,11,15,7];
+// let nums_after=nums.find(nums=>nums>10);
+// console.log(nums_after);
+
+// 6-masala
+// let tech=["HTML",'CSS','JS',"React"];
+// let tech_after=tech.includes('React');
+// console.log(tech_after);
+
+// 7-masala
+// let words=['js','javascript','html','css'];
+// let words_after=words.sort((a,b)=>a.length-b.length);
+// console.log(words_after);
+
+// 8-masala
+// let nums=[5,7,10,0,-3];
+// console.log(nums.every(nums=>nums>0));
+
+// 9-masala
+// let girls=['Laylo','Zarina'];
+// let boys=['Bekzod','Ali'];
+// console.log(girls.concat(boys));
+
+// 10-masala
+// let arr=[10,20,30,40,50];
+// let arr_after=arr.splice(1,2);
+// console.log(arr_after);
+
+// 11-masala
+// let fruits=['apple','banana','orange','apple'];
+// let fruits_after=fruits.lastIndexOf('apple');
+// console.log(fruits_after);
+
+// 12-masala
+// let nums=[3,8,11,14,17,20];
+// let nums_after=nums.filter(nums=>nums%2==0).map(nums=>nums*2);
+// console.log(nums_after);
+
+// 13-masala
+// let ages=[12,15,22,16];
+// console.log(ages.some(ages=>ages>18));
+
+// 14-masala
+let arr=['middle'];
+arr.push('end');
+arr.unshift('start');
+console.log(arr);
